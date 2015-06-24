48a9044c68b801c8a0cea1be5397bb716bf62117
dphans <dinophan94@icloud.com>
2015-06-24 15:55:39 +0700

    Released

--- DemoDPTheme/AppDelegate.swift
+++ DemoDPTheme/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applyTheme() {
         
-        var mainColor: UIColor = DPTheme.color(0x009688, alpha: 1.0)
+        var mainColor: UIColor = DPTheme.color(0x3F51B5, alpha: 1.0)
         var secondColor: UIColor = DPTheme.color(0xE0F2F1, alpha: 1.0)
         let fontName = "Avenir"
         
