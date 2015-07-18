9114e0e177c20b654091a57d4a6ee3adee401224
dphans <baophan94@icloud.com>
2015-07-18 23:41:13 +0700

    Update ViewController.swift

--- DemoDPTheme/ViewController.swift
+++ DemoDPTheme/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak let profileImage: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
